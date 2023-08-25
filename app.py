eeaa1279ab8fc6d385f6ea52006eb535f25de6f7
alexaldearroyo <alexaldearroyo@gmail.com>
2023-08-25 16:29:53 +0900

    Flask implementation working.

--- app.py
+++ app.py
@@ -3,5 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+
 def index():
     return render_template("index.html")
+
+@app.route('/dev')
+def dev():
+    return render_template('dev.html')
+
+@app.route('/texts')
+def texts():
+    return render_template('texts.html')
+
+@app.route('/more')
+def more():
+    return render_template('more.html')
+
+if __name__ == "__main__":
+    app.run()

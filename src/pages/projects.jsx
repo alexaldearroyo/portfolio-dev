31b9b5bd64fd09e9449e12e0777df329aebe2888
alexaldearroyo <alexaldearroyo@gmail.com>
2024-01-14 11:23:33 +0900

    Partial projects implemented

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -12,7 +12,7 @@ const ProjectsPage = () => {
     <div className="projects-page">
       <div className="total-wrapper">
         <div className="grid-wrapper">
-          {/* Bloque de contenido */}
+          {/* PRIORITIES */}
           <div className="centered-box-grid">
             <div className="box-header">Priorities</div>
             <div className={`box-content ${isExpanded ? "expanded" : ""}`}>
@@ -38,11 +38,11 @@ const ProjectsPage = () => {
                 <div className="expandible">
                   <div className="inner-text-dev">
                     <img
-                        src="https://raw.githubusercontent.com/alexaldearroyo/priorities/main/screenshots_tr.png"
-                        alt="Priorities Screenshot"
-                        className="w-full h-auto mx-auto mb-4 block"
+                      src="priorities.png"
+                      alt="Priorities Screenshot"
+                      className="w-full h-auto mx-auto mb-4 block"
                     />
-                    <p className="pt-0 pb-4">
+                    <p className="pt-4 pb-4">
                       "Priorities" is a task management application for macOS
                       with a color-changing interface based on task count: blue
                       for few tasks, yellow for some, and red for many. It
@@ -52,12 +52,13 @@ const ProjectsPage = () => {
                     </p>
 
                     <p className="pt-3 text-center">
-                        <a
-                            href="https://github.com/alexaldearroyo/priorities"
-                            target="_blank"
-                        >
-                            <span className="arrow no-hover">&gt;</span> More info on GitHub
-                        </a>
+                      <a
+                        href="https://github.com/alexaldearroyo/priorities"
+                        target="_blank"
+                      >
+                        <span className="arrow no-hover">&gt;</span> More info
+                        on GitHub
+                      </a>
                     </p>
                   </div>
                 </div>
@@ -74,7 +75,64 @@ const ProjectsPage = () => {
             </div>
           </div>
 
-          {/* Repetir para otros bloques */}
+          {/* VISUAL GIT */}
+
+          <div class="centered-box-grid">
+            <div class="box-header">Visual Git</div>
+            <div className={`box-content ${isExpanded ? "expanded" : ""}`}>
+              <div class="preview">
+                Git and GitHub made simpler through command line menus.
+                <p className="mt-5 flex justify-center">
+                  <img
+                    src="python.svg"
+                    alt="Python Logo"
+                    className="h-5 w-auto mr-2"
+                  />
+                  Python
+                  <img
+                    src="bash.svg"
+                    alt="Bash Logo"
+                    className="h-5 w-auto ml-2 mr-2"
+                  />{" "}
+                  Bash
+                </p>
+              </div>
+              {isExpanded && (
+                <div class="expandible">
+                  <div class="inner-text-dev">
+                    <img
+                      src="vigitsh.png"
+                      alt="Visual Git Screenshot"
+                      className="w-1/1.5 h-auto mx-auto mb-4 block"
+                    />
+                    <p class="pb-4">
+                      A terminal-based script designed to simplify your version
+                      control process with Git and GitHub. Leveraging textual
+                      outputs, Visual Git provides an intuitive experience that
+                      streamlines your workflow, making complex tasks easier to
+                      manage and understand.
+                    </p>
+                    <p class="pt-3 text-center">
+                      <a
+                        href="https://github.com/alexaldearroyo/visualgit"
+                        target="_blank"
+                      >
+                        <span class="arrow">&gt;</span> More info on GitHub
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              )}
+              <div className="flex justify-center">
+                <button
+                  onClick={toggleExpandibleSection}
+                  className="toggle-content"
+                >
+                  {isExpanded ? "-" : "+"}
+                </button>
+              </div>{" "}
+            </div>
+          </div>
         </div>
       </div>
     </div>

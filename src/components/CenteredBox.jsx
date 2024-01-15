5ad9aec642316bfcecfe77bdfd33d2a434522489
alexaldearroyo <alexaldearroyo@gmail.com>
2024-01-15 15:10:05 +0900

    Design improveed.

--- src/components/CenteredBox.jsx
+++ src/components/CenteredBox.jsx
@@ -20,138 +20,156 @@ function CenteredBox() {
       </p>
 
       <p className="text-center">
-        <span className="inline-block" style={{ margin: "5px 0" }}>
-          <img src="python.svg" alt="Python Logo" className="h-6 w-auto mr-2" />
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
+          <img src="python.svg" alt="Python Logo" className="ml-3 h-6 w-auto mr-3" />
+          <span className="hover-text">Python</span>
         </span>
         {/* Python  */}
-        <span className="inline-block" style={{ margin: "5px 0" }}>
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
           <img
             src="html.svg"
             alt="HTML Logo"
-            className="ml-3 h-6 w-auto mr-2"
+            className="ml-3 h-6 w-auto mr-3"
           />
+          <span className="hover-text">HTML 5</span>
         </span>
         {/* HTML 5  */}
-        <span className="inline-block" style={{ margin: "5px 0" }}>
-          <img src="css.svg" alt="CSS Logo" className="ml-3 h-6 w-auto mr-2" />
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
+          <img src="css.svg" alt="CSS Logo" className="ml-3 h-6 w-auto mr-3" />
+          <span className="hover-text">CSS 3</span>
         </span>
         {/* CSS 3  */}
-        <span className="inline-block" style={{ margin: "5px 0" }}>
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
           <img
             src="js.svg"
             alt="JavaScript Logo"
-            className="ml-3 h-6 w-auto mr-2"
+            className="ml-3 h-6 w-auto mr-3"
           />
+            <span className="hover-text">JavaScript</span>
         </span>
         {/* JavaScript  */}
-        <span className="inline-block" style={{ margin: "5px 0" }}>
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
           <img
             src="ts.svg"
             alt="TypeScript Logo"
-            className="ml-3 h-6 w-auto mr-2"
+            className="ml-3 h-6 w-auto mr-3"
           />
+          <span className="hover-text">TypeScript</span>
         </span>
         {/* TypeScript  */}
-        <span className="inline-block" style={{ margin: "5px 0" }}>
-          <img src="sql.svg" alt="SQL Logo" className="ml-3 h-6 w-auto mr-2" />
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
+          <img src="sql.svg" alt="SQL Logo" className="ml-3 h-6 w-auto mr-3" />
+          <span className="hover-text">SQL</span>
         </span>
         {/* SQL  */}
-        <span className="inline-block" style={{ margin: "5px 0" }}>
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
           <img
             src="bash.svg"
             alt="Bash Logo"
-            className="ml-3 h-6 w-auto mr-2"
+            className="ml-3 h-6 w-auto mr-3"
           />
+          <span className="hover-text">Bash</span>
         </span>
         {/* Bash  */}
 
-        <span className="inline-block" style={{ margin: "5px 0" }}>
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
           <img
             src="react.svg"
             alt="React Logo"
-            className="ml-3 h-6 w-auto mr-2"
+            className="ml-3 h-6 w-auto mr-3"
           />
+          <span className="hover-text">React.js</span>
         </span>
         {/* React.js  */}
-        <span className="inline-block" style={{ margin: "5px 0" }}>
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
           <img
             src="next.svg"
             alt="Next Logo"
-            className="ml-3 h-6 w-auto mr-2"
+            className="ml-3 h-6 w-auto mr-3"
           />
+          <span className="hover-text">Next.js</span>
         </span>
         {/* Next.js  */}
-        <span className="inline-block" style={{ margin: "5px 0" }}>
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
           <img
             src="node.svg"
             alt="Node Logo"
-            className="ml-3 h-6 w-auto mr-2"
+            className="ml-3 h-6 w-auto mr-3"
           />
+          <span className="hover-text">Node.js</span>
         </span>
         {/* Node.js  */}
-        <span className="inline-block" style={{ margin: "5px 0" }}>
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
           <img
             src="jquery.svg"
             alt="jQuery Logo"
-            className="ml-3 h-6 w-auto mr-2"
+            className="ml-3 h-6 w-auto mr-3"
           />
+          <span className="hover-text">jQuery</span>
         </span>
         {/* jQuery  */}
-        <span className="inline-block" style={{ margin: "5px 0" }}>
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
           <img
             src="vite.svg"
             alt="Vite Logo"
-            className="ml-3 h-6 w-auto mr-2"
+            className="ml-3 h-6 w-auto mr-3"
           />
+          <span className="hover-text">Vite</span>
         </span>
         {/* Vite  */}
 
-        <span className="inline-block" style={{ margin: "5px 0" }}>
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
           <img
             src="tailwind.svg"
             alt="Tailwind Logo"
-            className="ml-3 h-6 w-auto mr-2"
+            className="ml-3 h-6 w-auto mr-3"
           />
+          <span className="hover-text">Tailwind CSS</span>
         </span>
         {/* Tailwind CSS  */}
-        <span className="inline-block" style={{ margin: "5px 0" }}>
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
           <img
             src="bootstrap.svg"
             alt="Bootstrap Logo"
-            className="ml-3 h-6 w-auto mr-2"
+            className="ml-3 h-6 w-auto mr-3"
           />
+          <span className="hover-text">Bootstrap</span>
         </span>
         {/* Bootstrap  */}
-        <span className="inline-block" style={{ margin: "5px 0" }}>
+
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
           <img
             src="sass.svg"
             alt="Sass Logo"
-            className="ml-3 h-6 w-auto mr-2"
+            className="ml-3 h-6 w-auto mr-3"
           />
+          <span className="hover-text">Sass</span>
         </span>
         {/* Sass  */}
 
-        <span className="inline-block" style={{ margin: "5px 0" }}>
-          <img src="git.svg" alt="Git Logo" className="ml-3 h-6 w-auto mr-2" />
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
+          <img src="git.svg" alt="Git Logo" className="ml-3 h-6 w-auto mr-3" />
+          <span className="hover-text">Git</span>
         </span>
         {/* Git  */}
-        <span className="inline-block" style={{ margin: "5px 0" }}>
+        <span className="inline-block svg-icon hover-scale" style={{ margin: "5px 0" }}>
           <img
             src="jest.svg"
             alt="Jest Logo"
-            className="ml-3 h-6 w-auto mr-2"
+            className="ml-3 h-6 w-auto mr-3"
           />
+          <span className="hover-text">Jest</span>
         </span>
         {/* Jest  */}
         {/* <span className="inline-block" style={{ margin: '5px 0' }}>
-          <img src="figma.svg" alt="Figma Logo" className="ml-3 h-6 w-auto mr-2" />
+          <img src="figma.svg" alt="Figma Logo" className="ml-3 h-6 w-auto mr-3" />
         </span> */}
         {/* Figma  */}
         {/* <span className="inline-block" style={{ margin: '5px 0' }}>
           <img
             src="photoshop.svg"
             alt="Photoshop Logo"
-            className="ml-3 h-6 w-auto mr-2"
+            className="ml-3 h-6 w-auto mr-3"
           />
         </span> */}
         {/* Photoshop  */}
@@ -159,7 +177,7 @@ function CenteredBox() {
           <img
             src="illustrator.svg"
             alt="Illustrator Logo"
-            className="ml-3 h-6 w-auto mr-2"
+            className="ml-3 h-6 w-auto mr-3"
           />
         </span> */}
         {/* Illustrator  */}
@@ -167,7 +185,7 @@ function CenteredBox() {
 
       <p>
         – I am a creative type, so I make some{" "}
-        <a className="no-link-dec" href="https://alexarroyo-art.vercel.app/">
+        <a className="no-link-dec" href="https://alexarroyo-art.vercel.app/" target="_blank">
           <span className="highlight">
             art
             </span>
